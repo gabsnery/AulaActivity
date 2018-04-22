2cc43af70daf24de990fc1660d2189abcdda236c
gabsnery <gneri94@gmail.com>
2018-04-22 19:12:43 -0300

    Teste

--- app/src/main/java/com/example/gabi/aulaactivity/MainActivity.java
+++ app/src/main/java/com/example/gabi/aulaactivity/MainActivity.java
@@ -1,16 +1,67 @@
 package com.example.gabi.aulaactivity;
 
 import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private Button botao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(),"onCreate",Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_main); //metodo da classe activity, seta a view inicial da classe
 
+        botao = (Button) findViewById(R.id.botaoID);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SegundaActivity.class)); // Saindo dessa activity e indo para a segunda activity
+
+            }
+        });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(),"onStart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),"onRestart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"onPause",Toast.LENGTH_SHORT).show();    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(),"onStop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"on;destroy",Toast.LENGTH_SHORT).show();
+    }
 }
